af5970d1f13e658c8de23a44234c847f97ebbe8d
Marek Pelka <pelkam95@gmail.com>
2018-05-19 22:46:15 +0200

    Initial commit

---
+++ hasher.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	MD5Url    = "/md5/"
+	SHA1Url   = "/sha1/"
+	SHA224Url = "/sha224/"
+	SHA256Url = "/sha256/"
+	SHA384Url = "/sha384/"
+	SHA512Url = "/sha512/"
+	ALLURL    = "/all/"
+)
+
+func encode(b []byte) string {
+	return hex.EncodeToString(b[:])
+}
+
+func md5Handler(w http.ResponseWriter, r *http.Request) {
+	h := md5.Sum([]byte(r.URL.Path[len(MD5Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func sha1Handler(w http.ResponseWriter, r *http.Request) {
+	h := sha1.Sum([]byte(r.URL.Path[len(SHA1Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func sha224Handler(w http.ResponseWriter, r *http.Request) {
+	h := sha256.Sum224([]byte(r.URL.Path[len(SHA224Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func sha256Handler(w http.ResponseWriter, r *http.Request) {
+	h := sha256.Sum256([]byte(r.URL.Path[len(SHA256Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func sha384Handler(w http.ResponseWriter, r *http.Request) {
+	h := sha512.Sum384([]byte(r.URL.Path[len(SHA384Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func sha512Handler(w http.ResponseWriter, r *http.Request) {
+	h := sha512.Sum512([]byte(r.URL.Path[len(SHA512Url):]))
+	fmt.Fprintf(w, "%s", encode(h[:]))
+}
+
+func allHandler(w http.ResponseWriter, r *http.Request) {
+	wordTohash := []byte(r.URL.Path[len(ALLURL):])
+	m5 := md5.Sum(wordTohash)
+	s1 := sha1.Sum(wordTohash)
+	s224 := sha256.Sum224(wordTohash)
+	s256 := sha256.Sum256(wordTohash)
+	s384 := sha512.Sum384(wordTohash)
+	s512 := sha512.Sum512(wordTohash)
+	fmt.Fprintf(w, "%s\n"+
+		"%s\n"+
+		"%s\n"+
+		"%s\n"+
+		"%s\n"+
+		"%s\n", encode(m5[:]), encode(s1[:]), encode(s224[:]), encode(s256[:]), encode(s384[:]), encode(s512[:]))
+}
+
+func main() {
+	http.HandleFunc(MD5Url, md5Handler)
+	http.HandleFunc(SHA1Url, sha1Handler)
+	http.HandleFunc(SHA224Url, sha224Handler)
+	http.HandleFunc(SHA256Url, sha256Handler)
+	http.HandleFunc(SHA384Url, sha384Handler)
+	http.HandleFunc(SHA512Url, sha512Handler)
+	http.HandleFunc(ALLURL, allHandler)
+
+	log.Fatal(http.ListenAndServe(":8008", nil))
+}
---
+++ hasher_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+)
+
+type urlHashHandler func(w http.ResponseWriter, r *http.Request)
+
+var (
+
+	TEST_STRING = "String_for_testing"
+	MD5_RESPONSE = "1f9be8d2262152abbf9c595fe8651ce9"
+	SHA1_RESPONSE = "49f0edf87144e8aef8fcf43753cbd7a2497998b2"
+	SHA224_RESPONSE = "82022c87bb14169295b5b13688404f013f4c39011c204ae358bff579"
+	SHA256_RESPONSE = "77c307a66057925a284f6fe6346b5a89bd11e93be3a39e0da43b37fdf05d61d6"
+	SHA384_RESPONSE = "40aa6fb476e83ac0a82aac3484da942a5fa417bdf376f115298cfd28b9d0093cd282fa678d8549f3624108c0a27fb7bb"
+	SHA512_RESPONSE = "f9e6aa9514902a0362c64c9849b41bab1525d4d1732e8807de8a380015996eb6ab57e5a613845add6524f4cdd2dc5c9b8ac86343c1977eb8ae2fe150b8697771"
+)
+
+func HttpWraper(t *testing.T, function urlHashHandler, url string, response string)  {
+	req, err := http.NewRequest("GET", url+TEST_STRING, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(function)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := response
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestMD5(t *testing.T) {
+	HttpWraper(t, md5Handler, MD5Url, MD5_RESPONSE)
+}
+
+func TestSHA1(t *testing.T) {
+	HttpWraper(t, sha1Handler, SHA1Url, SHA1_RESPONSE)
+}
+
+func TestSHA224(t *testing.T) {
+	HttpWraper(t, sha224Handler, SHA224Url, SHA224_RESPONSE)
+}
+
+func TestSHA256(t *testing.T) {
+	HttpWraper(t, sha256Handler, SHA256Url, SHA256_RESPONSE)
+}
+
+func TestSHA384(t *testing.T) {
+	HttpWraper(t, sha384Handler, SHA384Url, SHA384_RESPONSE)
+}
+
+func TestSHA512(t *testing.T) {
+	HttpWraper(t, sha512Handler, SHA512Url, SHA512_RESPONSE)
+}
+
